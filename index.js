36443e4f7060b657916acfc195a1121fab52f033
Samuel-DeSantis <samuel.i.desantis@gmail.com>
2021-09-20 23:59:26 -0600

    commit tree 876dc58526eff7039fd03b3edccfe89f6c1bc6f9

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize (string) {
+    return string.replace( /[^A-Za-z0-9 '-]/g, '' )
+  }
+
+  static isException (word) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return Boolean(exceptions.find (exception => word === exception))
+  }
+
+  static titleize (string) {
+    return string.split(' ').map( (word, index) => {
+      if (index === 0) {return this.capitalize(word)} 
+      else if (this.isException(word)) {return word}
+      else {return this.capitalize(word)}
+    }).join(' ')
+  }
 }
